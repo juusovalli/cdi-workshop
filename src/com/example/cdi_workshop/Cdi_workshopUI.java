3d19338c83cb822f7f454075522418813a0ac2cb
Juuso Valli <juuso@vaadin.com>
2014-12-04 13:05:43 +0200

    Alter the UI to use CDI

--- src/com/example/cdi_workshop/Cdi_workshopUI.java
+++ src/com/example/cdi_workshop/Cdi_workshopUI.java
@@ -1,22 +1,15 @@
 package com.example.cdi_workshop;
 
-import javax.servlet.annotation.WebServlet;
-
 import com.vaadin.annotations.Theme;
-import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.cdi.CDIUI;
 import com.vaadin.navigator.Navigator;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.UI;
 
 @SuppressWarnings("serial")
 @Theme("cdi_workshop")
+@CDIUI("")
 public class Cdi_workshopUI extends UI {
-
-    @WebServlet(value = "/*", asyncSupported = true)
-    @VaadinServletConfiguration(productionMode = false, ui = Cdi_workshopUI.class)
-    public static class Servlet extends VaadinServlet {
-    }
 
     @Override
     protected void init(VaadinRequest request) {
